976babca269dc127f2e6b3549d346b79707bf0d6
Mills Provosty <millsprovosty@gmail.com>
2019-02-20 10:25:44 -0700

    first attempt at documenting

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -9,10 +9,17 @@ class Vendor
     @inventory = {}
   end
 
-  def stock(key, value)
+  def stock(key, new_value)
+    @inventory[key] = new_value
     #take in a Key and value Arg
     #create a new hash with Key Value pair
     #if the next key value arg has the same key, add the sum of the values
+    if @inventory.key == key
+
+      return @inventory.values.map do |value|
+        value += new_value
+      end
+    end
   end
 
   def check_stock(key)
