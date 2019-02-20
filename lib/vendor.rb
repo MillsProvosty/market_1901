df173d2ed2f6059fedc7a9f7789a7303cdca77ba
Mills Provosty <millsprovosty@gmail.com>
2019-02-20 10:18:40 -0700

    initial commit- can't figure out the stock method. Can make new hash, but can't update values

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,10 +1,27 @@
 class Vendor
   attr_reader :name,
-              :inventory
+              :inventory,
+              :check_stock,
+              :stock
 
   def initialize(name)
     @name = name
-    @inventory = # you decide what goes here...
+    @inventory = {}
+  end
+
+  def stock(key, value)
+    #take in a Key and value Arg
+    #create a new hash with Key Value pair
+    #if the next key value arg has the same key, add the sum of the values
+  end
+
+  def check_stock(key)
+    @inventory.include?(key)
+    if @inventory.values.empty? == true
+      return 0
+    else
+      return @inventory.values
+    end
   end
 
 end
