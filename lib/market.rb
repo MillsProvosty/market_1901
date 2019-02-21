0e3a7b870de412089615ba8b38610e01051dd4d9
Mills Provosty <millsprovosty@gmail.com>
2019-02-20 18:21:05 -0700

    More work

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,11 @@ class Market
     return names
   end
 
+  def vendors_that_sell(item_arg)
+    sellers = @vendors.group_by do |vendor_name, items_sold|
+      items_sold.inventory == items_arg
+    end
+    sellers
+  end
+
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -39,14 +39,22 @@ class MarketTest < Minitest::Test
   end
 
   def test_vendors_that_sell_method
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
     @vendor_1.stock("Peaches", 35)
     @vendor_1.stock("Tomatoes", 7)
     @vendor_2.stock("Banana Nice Cream", 50)
     @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
     @vendor_3.stock("Peaches", 65)
+binding.pry
     expected = [@vendor_1, @vendor_3]
     assert_equal expected, @market.vendors_that_sell("Peaches")
   end
+
 
 
 end
