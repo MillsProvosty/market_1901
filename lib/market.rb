8cc197682dc84e5f1c6f756b71b32d53c560783d
Mills Provosty <millsprovosty@gmail.com>
2019-02-20 18:03:43 -0700

    progress so far

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,20 @@
 class Market
+  attr_reader :name,
+              :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    names = @vendors.map do |vendor|
+      vendor.name
+    end
+    return names
+  end
 
 end
